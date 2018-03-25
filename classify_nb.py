94fb126425f65eb5c6ac41364fb250d323db4025
Anand Krishna Sunil <nanduzira@users.noreply.github.com>
2018-03-25 20:37:17 +0530

    Single sample prediction using Naive Bayes Module

--- classify_nb.py
+++ classify_nb.py
@@ -4,21 +4,32 @@ from sklearn import metrics
 from sklearn.naive_bayes import GaussianNB
 
 from csv_handler import load_csv
+import numpy as np
 
+filename = "temp.csv"
+trainer, target = load_csv(filename)
+
+# print (trainer, "\n", target)
 
 # load the datasets
-# dataset = datasets.load_iris()
+dataset = datasets.load_iris()
 # print (dataset.target)
 # print (type(dataset.data[]))
 
 # # fit a Naive Bayes model to the data
-# model = GaussianNB()
-# model.fit(dataset.data, dataset.target)
-# # print(model)
+model = GaussianNB()
+model.fit(trainer, target)
+print(model)
 
 # # make predictions
-# expected = dataset.target
-# predicted = model.predict(dataset.data)
+expected = target
+# test = np.asarray([0.2, 350.0, 3.9, 269.5, 100.0, 16.0, 86.0, 20.8, 8.3, 36.0, 0.09, 0.16, 9.975805169, 8.9, 1.0, 0.0])
+# test.reshape(1, -1)
+# print (test)
+predicted = model.predict([[0.2, 350.0, 3.9, 269.5, 100.0, 16.0, 86.0, 20.8, 8.3, 36.0, 0.09, 0.16, 9.975805169, 8.9, 1.0, 0.0]])
+print (predicted)
+# print (type(dataset.target))
+# print (type(np.asarray([0.2, 350.0, 3.9, 269.5, 100.0, 16.0, 86.0, 20.8, 8.3, 36.0, 0.09, 0.16, 9.975805169, 8.9, 1.0, 0.0])))
 
 # summarize the fit of the model
 # print(metrics.classification_report(expected, predicted))
--- csv_handler.py
+++ csv_handler.py
@@ -4,14 +4,20 @@ import numpy as np
 def load_csv(filename):
     lines = csv.reader(open(filename, "r"))
     dataset = list(lines)
-    print (dataset)
+    # print (dataset)
+    trainer = []
+    target = []
 
     for i in range(len(dataset)):
-        dataset[i] = [float(x) for x in dataset[i]]
+        # print (dataset[i])
+        trainer.append([float(dataset[i][j]) for j in range(len(dataset[i])-1)])
+        # print (dataset[i][16])
+        target.append(dataset[i][16])
 
-    return dataset
+    return trainer, target
 
-filename = "temp.csv"
-dataset = load_csv(filename)
-print (dataset)
-print ("Loaded data file {0} with {1} rows :", filename, "-", len(dataset))
+# filename = "temp.csv"
+# trainer, target = load_csv(filename)
+# print ("TRAINER", trainer)
+# print ("TARGET", target)
+# print ("Loaded data file {0} with {1} rows :", filename, "-", len(target))
